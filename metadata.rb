b3dbd2f491fb0c9273b02399031fcdf55744c504
Blake Irvin <bixv@icloud.com>
2014-10-22 17:48:27 +0200

    bump version for permissions bugfix

--- metadata.rb
+++ metadata.rb
@@ -4,5 +4,5 @@ maintainer_email 'mcamou@tecnoguru.com, blake.irvin@gmail.com'
 license          'MIT'
 description      'Installs/Configures the Scalable TLS Unwrapping Daemon'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
 depends		 'smf'
